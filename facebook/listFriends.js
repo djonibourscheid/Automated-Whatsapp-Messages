eda786f1ba0fcf08a9fbe529ddd405a56a493546
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-07-04 21:26:33 -0300

    Caso um amigo não exista não dará mais erro

--- facebook/listFriends.js
+++ facebook/listFriends.js
@@ -1,16 +1,25 @@
 const allFriends = document.querySelectorAll('div[data-pagelet="ProfileAppSection_0"] div.buofh1pr.hv4rvrfc');
 
 const friendsList = [];
+const invalidProfiles = [];
 
 allFriends.forEach(friend => {
-  const friendInfos = friend.querySelector('div a');
-  const linkProfile = friendInfos.getAttribute('href');
-  const nameProfile = friendInfos.querySelector('span').innerText;
+  try {
+    const friendInfos = friend.querySelector('div a');
+    const linkProfile = friendInfos.getAttribute('href');
+    const nameProfile = friendInfos.querySelector('span').innerText;
 
-  return friendsList.push({
-    name: nameProfile,
-    link: linkProfile
-  })
+    return friendsList.push({
+      nome: nameProfile,
+      link: linkProfile
+    })
+  }
+
+  catch (error) {
+    const nameProfileInvalid = friend.querySelector('div span').innerText;
+    return invalidProfiles.push({ name: nameProfileInvalid })
+  }
 })
 
 console.log(friendsList);
+console.log(invalidProfiles);
