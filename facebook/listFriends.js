cae439ce8dfc3cf9cad836cc5063636c62486a13
Djoni Bourscheid <djonibourscheid00@gmail.com>
2021-07-10 16:27:17 -0300

    Colocando o código em uma função
    Caso o usuário executar o código mais de uma vez não dará erro.

--- facebook/listFriends.js
+++ facebook/listFriends.js
@@ -1,28 +1,36 @@
-// retorna uma nodeList com as divs de cada amigo
-const allFriends = document.querySelectorAll('div[data-pagelet="ProfileAppSection_0"] div.buofh1pr.hv4rvrfc');
+function listFriends() {
+  // retorna uma nodeList com as divs de cada amigo
+  const allFriends = document.querySelectorAll('div[data-pagelet="ProfileAppSection_0"] div.buofh1pr.hv4rvrfc');
 
-const friendsList = [];
-const invalidProfiles = [];
+  const friendsList = [];
+  const invalidProfiles = [];
 
-allFriends.forEach(friend => {
-  try {
-    // pegando o link do perfil e o nome de cada pessoa.
-    const friendInfos = friend.querySelector('div a');
-    const linkProfile = friendInfos.getAttribute('href');
-    const nameProfile = friendInfos.querySelector('span').innerText;
+  allFriends.forEach(friend => {
+    try {
+      // pegando o link do perfil e o nome de cada pessoa.
+      const friendInfos = friend.querySelector('div a');
+      const linkProfile = friendInfos.getAttribute('href');
+      const nameProfile = friendInfos.querySelector('span').innerText;
 
-    return friendsList.push({
-      nome: nameProfile,
-      link: linkProfile
-    })
-  }
+      return friendsList.push({
+        nome: nameProfile,
+        link: linkProfile
+      })
+    }
 
-  catch (error) {
-    // Caso o perfil não exista, retorna apenas uma mensagem no console
-    const nameProfileInvalid = friend.querySelector('div span').innerText;
-    return invalidProfiles.push({ nome: nameProfileInvalid })
-  }
-})
+    catch (error) {
+      // Caso o perfil não exista, retorna apenas uma mensagem no console
+      const nameProfileInvalid = friend.querySelector('div span').innerText;
+      return invalidProfiles.push({ nome: nameProfileInvalid })
+    }
+  })
 
-console.log("Lista de amigos: ", friendsList);
-console.log("Lista de amigos que excluíram o perfil: ", invalidProfiles);
+  return (
+    console.log("Lista de amigos:"),
+    console.table(friendsList),
+    console.log("Perfis não encontrados:"),
+    console.table(invalidProfiles)
+  );
+}
+
+listFriends();
